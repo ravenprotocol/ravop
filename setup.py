504466b83b2565bddc2da64a957e79be7827a0dc
Kailash <kailash@matelabs.in>
2021-03-12 18:18:19 +0530

    Stats, tfjs and socket client added

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        "numpy==1.20.1"
+        "numpy==1.20.1",
+        "git+git://github.com/ravenprotocol/ravcom.git",
     ],
 )
