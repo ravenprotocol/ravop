b52859c96647fa0033b497608774f4598a3835a4
Kailash <ahikailash1@gmail.com>
2021-09-24 16:34:46 +0530

    Import: ravdb added

--- ravop/__init__.py
+++ ravop/__init__.py
@@ -3,6 +3,7 @@ from .config import QUEUE_LOW_PRIORITY, QUEUE_HIGH_PRIORITY, QUEUE_COMPUTING, RD
 from .utils import Singleton
 from .utils import dump_data, delete_data_file, save_data_to_file, inform_server, copy_data
 from .db import OpStatus, GraphStatus, ClientOpMappingStatus, Op, Graph, Data, Client, ClientOpMapping, DBManager, \
-    RavQueue, clear_redis_queues
+    RavQueue, clear_redis_queues, ravdb
 from .core import Op, Scalar, Tensor, t, Data, Graph, epsilon, one, minus_one, inf, pi, File
 from .globals import globals
+
