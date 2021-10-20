ad7487337c64f770ced4a7226e0aab90ced62914
Kailash <ahikailash1@gmail.com>
2021-10-20 11:37:59 +0530

    Setup.py updated

--- setup.py
+++ setup.py
@@ -5,18 +5,11 @@ setup(
     version='0.2-alpha',
     packages=find_packages(),
     install_requires=[
+        "SQLAlchemy_Utils==0.37.2",
         "numpy==1.20.1",
-        "numpy==1.20.1",
-        "SQLAlchemy==1.3.23",
         "redis==3.5.3",
-        "protobuf==3.15.5",
-        "six==1.15.0",
-        "aiohttp==3.6.2",
-        "async-timeout==3.0.1",
-        "python-engineio==3.13.0",
-        "python-socketio==4.5.1",
-        "requests==2.23.0",
-        "sqlalchemy-utils==0.37.2"
+        "SQLAlchemy==1.3.23",
+        "gevent_socketio==0.3.6"
     ],
     dependency_links=[
     ]
