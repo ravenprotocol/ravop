8deefe429e287c3e36872bd38573502e28386a84
Kailash <ahikailash1@gmail.com>
2021-11-10 17:51:26 +0530

    Module deleted: ravop/db

--- ravop/db/__init__.py
+++
@@ -1,5 +0,0 @@
-from .redis_manager import RavQueue, QUEUE_HIGH_PRIORITY, QUEUE_LOW_PRIORITY, QUEUE_COMPUTING, clear_redis_queues
-from .manager import DBManager
-from .models import Op, Graph, Data, Client, ClientOpMapping
-
-ravdb = DBManager.Instance()
