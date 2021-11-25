4a96723ff81cc6813cf61b4a2ddb8e723009a169
Unnikrishnan Menon <menon.uk1998@gmail.com>
2021-11-25 13:00:21 +0530

    minor fix with ravsock server url

--- ravop/config.py
+++ ravop/config.py
@@ -17,7 +17,7 @@ QUEUE_HIGH_PRIORITY = "queue:high_priority"
 QUEUE_LOW_PRIORITY = "queue:low_priority"
 QUEUE_COMPUTING = "queue:computing"
 
-RAVSOCK_SERVER_URL = os.environ.get("RAVSOCK_SERVER_URL", "http://0.0.0.0:9999")
+RAVSOCK_SERVER_URL = os.environ.get("RAVSOCK_SERVER_URL", "http://0.0.0.0:9999/")
 
 RDF_DATABASE_URI = "sqlite:///{}/rdf.db?check_same_thread=False".format(BASE_DIR)
 # RDF_DATABASE_URI = "mysql://root:qwerty12345.A@localhost/rdf"
