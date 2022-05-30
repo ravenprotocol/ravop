2614ef347c8a071b203658c34e03e12dc1316baf
Unnikrishnan Menon <menon.uk1998@gmail.com>
2022-05-30 12:59:38 +0530

    singleton import changed

--- ravop/__init__.py
+++ ravop/__init__.py
@@ -1,5 +1,6 @@
-from .utils import Singleton, dump_data, delete_data_file, save_data_to_file, inform, copy_data
+from .utils import dump_data, delete_data_file, save_data_to_file, inform, copy_data
 from .strings import Status, OpStatus, GraphStatus, MappingStatus
 from .core import Op, Scalar, Tensor, t, Data, Graph, epsilon, one, minus_one, inf, pi, File
 from .globals import globals
 from .core import *
+from .singleton_utils import Singleton
