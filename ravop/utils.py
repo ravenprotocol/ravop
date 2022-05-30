94523fd793193ac03ad0012550970653e8dfecc4
Unnikrishnan Menon <menon.uk1998@gmail.com>
2022-05-30 13:01:35 +0530

    removed singleton to resolve imports issue

--- ravop/utils.py
+++ ravop/utils.py
@@ -9,6 +9,7 @@ from .config import DATA_FILES_PATH, RAVSOCK_SERVER_URL, TEMP_FILES_PATH
 from .socket_client import SocketClient
 
 import pickle as pkl
+from .globals import globals as g
 
 def save_data_to_file(data_id, data):
     """
@@ -37,24 +38,6 @@ def delete_data_file(data_id):
     if os.path.exists(file_path):
         os.remove(file_path)
 
-
-class Singleton:
-    def __init__(self, cls):
-        self._cls = cls
-
-    def Instance(self):
-        try:
-            return self._instance
-        except AttributeError:
-            self._instance = self._cls()
-            return self._instance
-
-    def __call__(self):
-        raise TypeError('Singletons must be accessed through `Instance()`.')
-
-    def __instancecheck__(self, inst):
-        return isinstance(inst, self._cls)
-
 def copy_data(source, destination):
     try:
         shutil.copy(source, destination)
@@ -76,6 +59,7 @@ def inform():
 
 
 def make_request(endpoint, method, payload={}, headers=None):
+    headers = {"Authorization" : g.ravenverse_token}
     if method == "post":
         return requests.post(
             "{}{}".format(RAVSOCK_SERVER_URL, endpoint), json=payload, headers=headers
